2726d5f60c4eae8803fc0425b104ac365e37c5ec
Andrzej Markiewicz <endercom502@gmail.com>
2022-07-16 08:12:55 +0200

    task

---
+++ js/task-02.js
@@ -0,0 +1,24 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+function addElement() {
+    start = document.createElement("li");
+    start.innerHTML = "<h2>twu</h2>";
+
+
+}
+
+// const ula = start.innerHTML(ingredients);
+// // const newLi = start.createElement("li");
+// // newLi.textContent = ingredients;
+// // for (const ingredient of ingredients) {
+      
+
+// // }
+console.log();
